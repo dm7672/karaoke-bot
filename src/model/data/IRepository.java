65c8e43efdc8f93ec382364a97c167d3e14dee5e
dm7672 <dmitrsem10@gmail.com>
2025-10-23 23:07:03 +0500

    Создал интерфейс базы данных

---
+++ src/model/data/IRepository.java
@@ -0,0 +1,11 @@
+package model.data;
+import java.util.List;
+
+public interface IRepository<T, ID> {
+    List<T> findAll();
+    T      findById(ID id);
+    boolean existsById(ID id);
+    void   save(T entity);
+    void   update(T entity);
+    void   delete(ID id);
+}
